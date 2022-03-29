530d34e65ed992029818166e476b1f8cce32b165
Uiyx <uiyx@disroot.org>
2022-03-29 20:03:36 +0300

    Rainbow and automatic pairs

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(elfeed-org elfeed dashboard eglot lsp-mode evil-nerd-commenter smooth-scrolling evil-collection evil use-package undo-tree)))
+   '(rainbow-delimiters elfeed-org elfeed dashboard eglot lsp-mode evil-nerd-commenter smooth-scrolling evil-collection evil use-package undo-tree)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
