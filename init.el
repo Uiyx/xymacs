382058441635da47d7dc91be055489226429f58b
Uiyx <uiyx@disroot.org>
2022-03-29 23:40:12 +0300

    Small changes

--- init.el
+++ init.el
@@ -7,8 +7,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(electric-pair-mode t)
  '(package-selected-packages
-   '(rainbow-delimiters elfeed-org elfeed dashboard eglot lsp-mode evil-nerd-commenter smooth-scrolling evil-collection evil use-package undo-tree)))
+   '(slime-autoloads slime rainbow-delimiters elfeed-org elfeed dashboard eglot lsp-mode evil-nerd-commenter smooth-scrolling evil-collection evil use-package undo-tree)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
