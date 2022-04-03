561685e4ebf57fa53fb5479c2cfc00bd359e9bfe
Uiyx <uiyx@disroot.org>
2022-04-03 21:46:56 +0300

    Small setting additions

--- init.el
+++ init.el
@@ -2,3 +2,18 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("717524c69b467ea4af46a25846c1011ca67d807f53c059b47601a88475ae34b1" default))
+ '(package-selected-packages
+   '(rainbow-mode VampDark-theme elgot orderless company ws-butler which-key use-package undo-tree smooth-scrolling rainbow-delimiters projectile neotree magit haskell-mode general evil-nerd-commenter evil-collection elfeed-org eglot doom-themes doom-modeline dashboard corfu)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
