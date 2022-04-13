79908a29bf178c66e65e9cc9da33c378196d22fc
Uiyx <uiyx@disroot.org>
2022-04-13 21:47:41 +0300

    cleaned init.el

--- init.el
+++ init.el
@@ -2,16 +2,3 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(emacs-everywhere vterm ws-butler which-key vampyricdark-theme use-package undo-tree smooth-scrolling rust-mode rainbow-delimiters projectile orderless neotree magit lua-mode haskell-mode go-mode general evil-nerd-commenter evil-collection elfeed-org eglot doom-modeline dashboard corfu)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
