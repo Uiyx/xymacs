4d2e2383cfc304bfc4572b3ff9a32870b64dc0f3
Uiyx <uiyx@disroot.org>
2022-06-13 00:30:33 +0300

    Updates

--- init.el
+++ init.el
@@ -2,16 +2,3 @@
  (expand-file-name
   "config.org"
   user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(smart-compile mode-compile zig-mode ws-butler which-key vterm vampyricdark-theme use-package undo-tree sudo-edit smooth-scrolling rust-mode rainbow-delimiters projectile orderless neotree magit lua-mode haskell-mode go-mode general evil-nerd-commenter evil-collection emacs-everywhere elfeed-org eglot doom-modeline dashboard corfu catppuccin-theme auto-sudoedit all-the-icons)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
